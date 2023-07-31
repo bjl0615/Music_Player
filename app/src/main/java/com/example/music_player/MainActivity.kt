d9f0649e3d17ce8eea7bbc6f20c860eafb6be546
admin <bjl0615@gmail.com>
2023-08-01 01:28:49 +0900

    Media Player fore·ground Service Part2

--- app/src/main/java/com/example/music_player/MainActivity.kt
+++ app/src/main/java/com/example/music_player/MainActivity.kt
@@ -2,14 +2,12 @@ package com.example.music_player
 
 import android.content.Intent
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private var mediaPlayer : MediaPlayer? = null
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,27 +18,26 @@ class MainActivity : AppCompatActivity() {
         binding.stopButton.setOnClickListener { mediaPlayerStop() }
     }
 
-    private fun mediaPlayerStop() {
-        val intent = Intent(this , MediaPlayerService::class.java)
-            .apply {
-                action = MEDIA_PLAYER_STOP
-            }
+    private fun mediaPlayerPlay() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
         startService(intent)
     }
 
     private fun mediaPlayerPause() {
-        val intent = Intent(this , MediaPlayerService::class.java)
-            .apply {
-                action = MEDIA_PLAYER_PAUSE
-            }
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
         startService(intent)
     }
 
-    private fun mediaPlayerPlay() {
-        val intent = Intent(this , MediaPlayerService::class.java)
-            .apply {
-                action = MEDIA_PLAYER_PLAY
-            }
+    private fun mediaPlayerStop() {
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
         startService(intent)
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, MediaPlayerService::class.java))
+        super.onDestroy()
     }
 }
