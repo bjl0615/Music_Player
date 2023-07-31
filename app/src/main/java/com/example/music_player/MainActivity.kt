e34fd3fbb6c3ed59368f9d7dd4ee4f76669524cc
admin <bjl0615@gmail.com>
2023-07-31 17:43:45 +0900

    Media Play

--- app/src/main/java/com/example/music_player/MainActivity.kt
+++ app/src/main/java/com/example/music_player/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.music_player
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var mediaPlayer : MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
+    }
+
+    private fun mediaPlayerStop() {
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    private fun mediaPlayerPause() {
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerPlay() {
+        if(mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this , R.raw.cheer).apply {
+                isLooping = true
+            }
+            mediaPlayer?.start()
+        }
     }
 }
